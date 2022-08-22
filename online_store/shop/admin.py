578c7c8f6cd758fd942f1f14d2bd478aa0f57c13
marufatoev <marufatoev@gmil.com>
2022-08-22 23:55:10 +0500

    shop

---
+++ online_store/shop/admin.py
@@ -0,0 +1,15 @@
+from itertools import product
+from django.contrib import admin
+from .models import *
+
+
+class SlugAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Product, SlugAdmin)
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Slide)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
